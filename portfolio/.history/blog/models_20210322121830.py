a78dde94d754d90ab0d10a17c5bad6d05554c3b2
steelerx155 <steelerx155@gmail.com>
2021-03-22 12:39:16 -0400

    added blog model

--- portfolio/.history/blog/models_20210322121830.py
+++ portfolio/.history/blog/models_20210322121830.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
-    title = models.CharField(max_length=250)
-    pub_date = models.DateTimeField()
-    content = models.TextField()
+    title = models.CharField
+    pub_date = models.DateField
+    content = models.TextField
     image = models.ImageField(upload_to='images/')
     
 
