30cc4a3bb2d493e75d437cade1c49feaac092447
steelerx155 <steelerx155@gmail.com>
2021-03-26 09:50:55 -0400

    added github and heroku urls to job. Updated blog and twitter links

--- portfolio/.history/jobs/models_20210326093416.py
+++ portfolio/.history/jobs/models_20210326093416.py
@@ -5,7 +5,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=1000)
-    heroku_url = models.URLField(max_length=100, null=True)
-    github_url = models.URLField(max_length=100, null=True)
+    heroku_url = models.URLField(max_length=100)
+    github_url = models.URLField(max_length=100)
 
 
