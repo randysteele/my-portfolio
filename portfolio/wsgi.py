93f7f15e112a8479b1546ae52842dccbe207b62a
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2021-03-30 13:53:17 -0400

    Update wsgi.py

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -9,11 +9,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from portfolio.wsgi import PortfolioApplication
-application = PortfolioApplication(application)
+
