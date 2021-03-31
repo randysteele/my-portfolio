26a57056eb8896a9c9781f06ec8c049ca40181b7
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2021-03-31 12:23:20 -0400

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 ]
 
 DATABASE_URL = 'postgresql://<postgresql>'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
