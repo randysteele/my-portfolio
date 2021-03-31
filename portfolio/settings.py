b25b1680c250d17d847cafcb385df297d8c84ca3
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2021-03-31 18:09:56 -0400

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -213,7 +213,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':'portfoliodb',
         'USER': 'randysteele',
         'PASSWORD':'admin',
