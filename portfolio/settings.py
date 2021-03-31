371f080cfa7241501b70fb170ec4a0c2eb88ee7b
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2021-03-31 12:31:23 -0400

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATICFILES_DIRS = ( 
-    os.path.join(BASE_DIR, 'portfolio/static/')
+    os.path.join(BASE_DIR, 'static')
 )
 
 DATABASE_URL = 'postgresql://<postgresql>'
