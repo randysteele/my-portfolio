6afc15a1ed652309dee9330a1b574e587e2e52c7
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2021-03-31 09:07:46 -0400

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 import dj_database_url
 django_heroku.settings(locals())
 DATABASES['default'] = dj_database_url.config()
-
+DATABASE_URL = 'postgresql://<postgresql>'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
