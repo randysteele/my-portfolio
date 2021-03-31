c19e8a3421218fd985840d3c51155c6ad6d170df
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2021-03-31 08:34:54 -0400

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -151,6 +151,6 @@ try:
 except ImportError:
     pass
 
-# django_heroku.settings(locals())
-# import dj_database_url
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+django_heroku.settings(locals())
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
